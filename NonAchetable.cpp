e991e7390afb6b1eb97ad2021c02e6fc271feb43
Sacha Cantat <>
2021-04-23 20:49:20 +0000

    maj

--- NonAchetable.cpp
+++ NonAchetable.cpp
@@ -1,42 +1,29 @@
+/**
+ * @file        NonAchetable.cpp
+ * @brief       Contient la déclaration de la classe NonAchetable
+ * @details     La classe NonAchetable est
+ * @author      Sacha CANTAT
+ * @date        17/04/21
+ */
+
+#include <iostream>
+#include <stdlib.h>
+#include "CaseMonop.hpp"
+#include "NonAchetable.hpp"
+#include "Joueur.hpp"
 
 
 
-main.out: main.o Joueur.o CaseMonop.o Achetable.o NonAchetable.o Constructible.o  Gare.o Chance.o Depart.o Prison.o Plateau.o
-	g++ -o main.out main.o Joueur.o CaseMonop.o Achetable.o NonAchetable.o Constructible.o  Gare.o Chance.o Depart.o Prison.o Plateau.o
+using namespace std;
 
 
-Achetable.o: Achetable.cpp Achetable.hpp CaseMonop.hpp Joueur.hpp
-	g++ -c Achetable.cpp
-CaseMonop.o: CaseMonop.cpp CaseMonop.hpp
-	g++ -c CaseMonop.cpp
-Constructible.o: Constructible.cpp Constructible.hpp
-	g++ -c Constructible.cpp
-Gare.o: Gare.cpp Gare.hpp
-	g++ -c Gare.cpp
+NonAchetable::NonAchetable(): CaseMonop()
+{
 
-Joueur.o: Joueur.cpp Joueur.hpp
-	g++ -c Joueur.cpp
+}
+NonAchetable::~NonAchetable(){}
 
-
-Chance.o: Chance.cpp Chance.hpp
-	g++ -c Chance.cpp
-
-Depart.o: Depart.cpp Depart.hpp
-	g++ -c Depart.cpp
-
-NonAchetable.o: NonAchetable.cpp NonAchetable.hpp
-	g++ -c NonAchetable.cpp
-
-Plateau.o: Plateau.cpp Plateau.hpp
-	g++ -c Plateau.cpp
-
-
-Prison.o: Prison.cpp Prison.hpp
-	g++ -c Prison.cpp
-
-main.o: main.cpp Joueur.hpp Achetable.hpp CaseMonop.hpp Constructible.hpp Gare.hpp Plateau.hpp Depart.hpp
-	g++ -c main.cpp
-
-
-clean : 
-	rm *.o
+void NonAchetable::affiche() const
+{
+ cout <<"achetable"<<endl;
+}
